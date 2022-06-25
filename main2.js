ffcb55e710f96521802b1c24f980ea358e792fbd
Alejandra <machate47@misena.edu.co>
2022-06-25 12:45:15 -0500

    commit 1

---
+++ main2.js
@@ -0,0 +1,19 @@
+let elementos = ''
+
+fetch('https://jsonplaceholder.typicode.com/photos')
+.then(response => {
+    // console.log(response.json());
+    return response.json();
+})
+.then(data => {
+    // console.log(data);
+    for (let i = 0; i < data.length; i++) {
+        console.log(data[i])
+            elementos += `<li>${data[i].id}-
+            <img src="${data[i].thumbnailUrl}">
+            ${data[i].title}
+            </li>`; 
+            console.log(data[i].title);
+        }
+        document.getElementById('posts').innerHTML = elementos;
+    })
